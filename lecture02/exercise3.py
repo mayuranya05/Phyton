c501d867a872971869b7d8c03a635eef81f2a3ed
mayuranya05 <mayuranya1485@gmail.com>
2023-07-20 19:21:38 +0700

    Lecture002

---
+++ lecture02/exercise3.py
@@ -0,0 +1,12 @@
+score_test = int(input("Enter you score test :"))
+if score_test >= 90:
+    print("Grade A")
+elif score_test >= 80:
+    print("Grade B")
+elif score_test >= 70:
+    print("Grade C")
+elif score_test >= 60:
+    print("Grade D")
+else:
+    print("Grade F")
+    
