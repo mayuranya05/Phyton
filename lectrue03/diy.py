77e0e5c5d25d8f9321d862525f7efcba8bf7b1c4
mayuranya05 <mayuranya1485@gmail.com>
2023-07-25 16:58:19 +0700

    Hello

--- lectrue03/diy.py
+++ lectrue03/diy.py
@@ -1,5 +1,5 @@
 print('---------------------------------------------------------')
-print('   27    \t    28    \t    29    \t    30')
+print('    27   \t    28     \t      29    \t      30')
 print('---------------------------------------------------------')
 
 
@@ -8,4 +8,4 @@ for num in range(1, 13,):
     twentyeight = num * 28
     twentynine  = num * 29
     thirty = num * 30
-    print(num , '\t',twentyseven, '\t', num, '\t',twentyeight, '\t', num, '\t',twentynine, '\t',num, '\t',thirty)
+    print(num ,'\t',twentyseven,'\t',num,'\t',twentyeight,'\t',num,'\t',twentynine,'\t',num,'\t',thirty)
