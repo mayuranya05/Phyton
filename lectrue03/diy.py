231db89375854b64518cb7a56e201544632b86f3
mayuranya05 <mayuranya1485@gmail.com>
2023-07-25 14:29:50 +0700

    multiplication_table

---
+++ lectrue03/diy.py
@@ -0,0 +1,11 @@
+print('---------------------------------------------------------')
+print('   27    \t    28    \t    29    \t    30')
+print('---------------------------------------------------------')
+
+
+for num in range(1, 13,):
+    twentyseven = num * 27
+    twentyeight = num * 28
+    twentynine  = num * 29
+    thirty = num * 30
+    print(num , '\t',twentyseven, '\t', num, '\t',twentyeight, '\t', num, '\t',twentynine, '\t',num, '\t',thirty)
