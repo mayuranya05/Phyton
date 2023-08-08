0e841e3ef5e510d8056347e2c8c3570cdc16b354
mayuranya05 <mayuranya1485@gmail.com>
2023-08-09 00:01:41 +0700

    I can do it

--- lecture04/exercise1.py
+++ lecture04/exercise1.py
@@ -2,8 +2,8 @@ first_name = str(input("Enter your first name : "))
 last_name = str(input("Enter your last name : "))
 student_id = str(input("Enter your study ID number : "))
 
-system_first_name = first_name[-3::]
-system_last_name = last_name[-3::]
+system_first_name = first_name[0:3:]
+system_last_name = last_name[0:3:]
 system_study_id = student_id[-3::]
 
 print(system_first_name + system_last_name + system_study_id)
