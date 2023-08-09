8e8937fbac262dd09c72a003a225f165d2ce1526
mayuranya05 <mayuranya1485@gmail.com>
2023-08-09 14:17:39 +0700

    Hello

--- lectrue05/reclusive.py
+++ lectrue05/reclusive.py
@@ -1,4 +1,15 @@
 def main():
+    # Get a number from the user.
     number = int(input('Enter a nonnegative integer : '))
-
+    # Get the factorial of the number.
     fact = factorial(number)
+    print('The factorial of', number, 'is', fact)
+    # Display the factorial.
+    
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num -1)
+
+main()
