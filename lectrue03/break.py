2c64aa33e2912e50a2e9483da41ae5c909461642
mayuranya05 <mayuranya1485@gmail.com>
2023-08-22 18:49:56 +0700

    Helllo

--- lectrue03/break.py
+++ lectrue03/break.py
@@ -1,5 +1,5 @@
 # Prints all letters except 'm' and 't'
 for letter in 'Mayuranya Tabsawat' :
-    if letter == 'm' or letter == 'r' :
+    if letter == 'm' or letter == 'n' :
         break
     print('Current Letter :', letter)
--- lecture02/if-elif-else2.py
+++ lecture02/if-elif-else2.py
@@ -1,19 +1,13 @@
 inchar = input("Input one character:")
-<<<<<<< HEAD
 
-=======
->>>>>>> c61ecf32278a6d5cbc13009ccb88bad8a46e5642
 if inchar >= 'A' and inchar <= 'Z':
     print("You in put Upper Case Letter", inchar)
+
 elif inchar >= 'a' and inchar <= 'z':
     print("You in put Lower Case Letter", inchar)
-<<<<<<< HEAD
+
 elif inchar >= '0' and inchar <= '9' :
     print("You in put Number", inchar)
-else :
-=======
-elif inchar >= '0' and inchar <= '9':
-    print("You in put Number", inchar)
+
 else:
->>>>>>> c61ecf32278a6d5cbc13009ccb88bad8a46e5642
     print("It's not a letter or number.", inchar)
