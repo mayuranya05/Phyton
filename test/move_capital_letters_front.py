9108aed2ecc42acfbb236b3e193414289fc51109
mayuranya05 <mayuranya1485@gmail.com>
2023-08-23 11:29:55 +0700

    test

---
+++ test/move_capital_letters_front.py
@@ -0,0 +1,8 @@
+def capToFront(string):
+    uppercase = 'Combine to uppercase for letters'
+    lowercase = 'Combine to lowercase for letters'
+    word = lowercase + uppercase
+
+print(capToFront("hApPy"))
+print(capToFront("moveMENT"))
+print(capToFront("shOrtCAKE"))
---
+++ test/้highest_digit.py
@@ -0,0 +1,6 @@
+def highestDigit(num):
+    return [num%10]
+
+print(highestDigit(379))
+print(highestDigit(2))
+print(highestDigit(377401))
