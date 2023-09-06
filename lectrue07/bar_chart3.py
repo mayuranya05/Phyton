8bd791f9190988316265e730d15c6069f8d5221c
mayuranya05 <mayuranya1485@gmail.com>
2023-09-06 10:29:26 +0700

    123

--- lectrue07/bar_chart3.py
+++ lectrue07/bar_chart3.py
@@ -1,24 +1,19 @@
 import matplotlib.pyplot as plt
 
 def main():
+    left_edges = [0, 10, 20, 30, 40]
+    heights = [100, 200, 300, 400, 500]
+    bar_widtg = 10
 
-    x_coords = [0, 1, 2, 3, 4]
-    y_coords = [0, 3, 1, 5, 2]
-
-    plt.plot(x_coords, y_coords, marker='o')
-
+    plt.bar(left_edges, heights, bar_widtg, color=('r', 'g', 'b', 'w', 'k'))
     plt.title('Sales by Year')
-
     plt.xlabel('Year')
     plt.ylabel('Sales')
 
-    plt.xticks([0, 1, 2, 3, 4],
+    plt.xticks([5, 15, 25, 35, 45],
                ['2016', '2017', '2018', '2019', '2020'])
-    plt.yticks([0, 1, 2, 3, 4, 5],
+    plt.yticks([0, 100, 200, 300, 400, 500],
                ['$0m', '$1m', '$2m', '$3m', '$4m', '$5m'])
-    
-    plt.grid(True)
-    
     plt.show()
 
 main()
