9e799e38d68ea19aaddb85e9d6ed6d191d38607b
mayuranya05 <mayuranya1485@gmail.com>
2023-09-11 10:59:06 +0700

    123

---
+++ lectrue08/unpickle_objects.py
@@ -0,0 +1,23 @@
+import pickle
+
+def main():
+    end_of_file = False
+
+    input_file = open('info.dat', 'rb')
+
+    while not end_of_file:
+        try:
+            person = pickle.load(input_file)
+
+            display_data(person)
+        except E0FError:
+            end_of_file = True
+
+    input_file.close()
+
+def display_data(person):
+    print('Name : ', person['name'])
+    print("Age : ", person['age'])
+    print('Weight : ', person['weight'])
+
+main()
